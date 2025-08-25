aacf283ee1fce2e9ef552de4b4b4106fd97e761b
pirozheka <pirozheka@gmail.com>
2025-08-25 19:41:23 +0500

    cleaning

--- src/main.js
+++ src/main.js
@@ -4,13 +4,3 @@ import './css/styles.css';
 import './js/playlist.js';
 import './js/droide.js';
 
-
-// улучшить карту теней
-const sceneEl = document.querySelector('a-scene');
-sceneEl.addEventListener('loaded', () => {
-    const sun = document.querySelector('#sun').getObject3D('light');
-    if (sun && sun.shadow) {
-        sun.shadow.mapSize.set(2048, 2048);
-        sun.shadow.bias = -0.0001;
-    }
-});
